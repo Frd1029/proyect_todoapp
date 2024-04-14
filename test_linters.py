a02b26b47af08d42a2560edf14261eba5f1076d7
omegablacks <jezael.calderon@gmail.com>
2024-04-13 20:48:42 -0500

    Feat: Linter Black installed

--- test_linters.py
+++ test_linters.py
@@ -1,9 +1,10 @@
 # Función para calcular la suma de los primeros n números naturales
 def suma_naturales(n):
     suma = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         suma += i
     return suma
+
 
 # Calcular la suma de los primeros 10 números naturales
 resultado = suma_naturales(10)
