517710f9a4d5cc37f3827897b2c3e9d672f97adf
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-04-23 23:46:57 -0500

    chore: correction to endpoint tasks_list

--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -3,6 +3,6 @@ from django.shortcuts import path;
 from . import views
 
 urlpatterns = [
-    path("tasks_list", views.tasks_list, name="tasks_list"),
+    path("tasks", views.tasks_list, name="tasks_list"),
     path("users", views.users_list, name="users_list")
 ]
