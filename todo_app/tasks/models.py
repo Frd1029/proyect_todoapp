ebad0e7b5c48a5a95e29f1e2c236cf6eafa907a2
omegablacks <jezael.calderon@gmail.com>
2024-04-23 22:05:29 -0500

    Chore: Update issues models CharField

--- todo_app/tasks/models.py
+++ todo_app/tasks/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.CharField(20)
+    status = models.CharField(max_length=20)
     deadline = models.DateField()
