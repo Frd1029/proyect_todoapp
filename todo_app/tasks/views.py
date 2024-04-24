35a5def77d3152b30854fc9b7acc502bf8ba6993
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-04-23 23:25:39 -0500

    chore: creation of methods listing

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Task, User
+
+def tasks_list(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'tasks/index.html', context)
+
+def users_list(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'tasks/index.html', context)
