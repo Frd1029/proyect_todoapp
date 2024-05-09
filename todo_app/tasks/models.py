71798322ff408d41f4af97b3021845f202894cfc
Frd1029 <freduar1029@gmail.com>
2024-05-09 11:55:37 -0500

    chore: The Task model is modified, correcting the status, and adding the Django users.

--- todo_app/tasks/models.py
+++ todo_app/tasks/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS_OPTIONS = {
+    "COMPLETED": "COMPLETED",
+    "PENDING": "PENDIENTE",
+    "DELAYED": "DELAYED",
+}
 
 
 # Create your models here.
 class Task(models.Model):
+
     title = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=10, choices=STATUS_OPTIONS, default="PENDING")
     deadline = models.DateField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        limit_choices_to={"is_superuser": False},
+        related_name="tasks",
+        null=True,
+    )
 
+    def __str__(self):
+        return self.title
