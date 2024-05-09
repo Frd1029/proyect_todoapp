3dd77c6ef905590214930ffd41454704b3a2a733
Frd1029 <freduar1029@gmail.com>
2024-05-09 11:53:27 -0500

    chore: Imports and references from the User model are removed.

--- todo_app/tasks/admin.py
+++ todo_app/tasks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Task, User
+from .models import Task
 
 admin.site.register(Task)
-admin.site.register(User)
+# admin.site.register(User)
