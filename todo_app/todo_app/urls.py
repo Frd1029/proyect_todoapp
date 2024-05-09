43daf4c03ccc6463ccff2522bfea8e6475af09a2
Frd1029 <freduar1029@gmail.com>
2024-05-09 08:28:47 -0500

    chore: Added routing to the rest-framework.

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('tasks/', include('tasks.urls')),
+    path('api-auth/', include('rest_framework.urls'))
+    #path('tasks/', include('tasks.urls')),
 ]
