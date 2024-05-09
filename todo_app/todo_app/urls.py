12b75433b126594bce3127f38a196223e1e2c012
Frd1029 <freduar1029@gmail.com>
2024-05-09 12:06:33 -0500

    feat: Rest-framework is applied to the project urls file.

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from tasks.views import TaskViewSet
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
-    #path('tasks/', include('tasks.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
