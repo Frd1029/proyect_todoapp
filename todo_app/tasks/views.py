60f53491272a7ca717df4ff16c3ed354732222d7
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-05-16 09:57:55 -0500

    chore: overwriting the methods of the TaskViewSet class

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -5,10 +5,28 @@ from .models import Task
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from .serializers import TasksSerializer
-
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.filters import OrderingFilter
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    #queryset = Task.objects.all()
     serializer_class = TasksSerializer
+    filter_backends = [OrderingFilter]
+    ordening_fields = ['priority']
+
+
+    def get_queryset(self):
+        return self.request.user.tasks.all()
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data["user"] = self.request.user
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CRATED, headers=headers)
+    
