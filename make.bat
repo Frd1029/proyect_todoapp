a3bcafefd5bf6c96ef85cdca662252a9b588d5ee
Frd1029 <freduar1029@gmail.com>
2024-05-16 09:25:16 -0500

    chore: Update make.bat, migrate and deactivate.

--- make.bat
+++ make.bat
@@ -10,7 +10,8 @@ if "%1"=="runserver" (
 rem Command to apply migrations
 if "%1"=="migrate" (
     call .\venv\Scripts\activate.bat
-    
+    cd .\todo_app
+    py manage.py migrate
 )
 
 rem Command to lint files with flake8
@@ -26,4 +27,9 @@ if "%1"=="lint" (
 rem Command Activate venv
 if "%1"=="activate" (
     call .\venv\Scripts\activate.bat
+)
+
+rem Command deactivate venv
+if "%1"=="deactivate" (
+    call .\venv\Scripts\deactivate.bat
 )
