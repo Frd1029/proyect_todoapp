bc122bc1b9814c8fde45926e65db5db0cc3b2f95
Frd1029 <freduar1029@gmail.com>
2024-05-21 08:30:33 -0500

    chore: Applying Linter to the views.py file.

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -12,15 +12,14 @@ from rest_framework.filters import OrderingFilter
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    #queryset = Task.objects.all()
+    queryset = Task.objects.all()
     serializer_class = TasksSerializer
     filter_backends = [OrderingFilter]
-    ordening_fields = ['priority']
-
+    ordening_fields = ["priority"]
 
     def get_queryset(self):
         return self.request.user.tasks.all()
-    
+
     def create(self, request, *args, **kwargs):
         data = request.data
         data["user"] = self.request.user
@@ -29,4 +28,3 @@ class TaskViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CRATED, headers=headers)
-    
