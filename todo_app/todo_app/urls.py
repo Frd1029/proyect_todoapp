8a27cbb2e4eed8fa61150af4d0a63585310e6501
Frd1029 <freduar1029@gmail.com>
2024-05-23 16:51:55 -0500

    chore: Update urls.

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -18,17 +18,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
 
-
+from rest_framework.authtoken import views
 from tasks.views import TaskViewSet
 
 router = routers.DefaultRouter()
 router.register('tasks', TaskViewSet)
 
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api/token/', obtain_auth_token)
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', views.obtain_auth_token),
+    path("admin/", admin.site.urls)
 ]
