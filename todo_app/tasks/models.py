f66e1b5d31b74dbd694e868ba1ec710fb24432cb
Frd1029 <freduar1029@gmail.com>
2024-05-23 16:05:48 -0500

    chore: Removal of the 'completed' field which is not necessary.

--- todo_app/tasks/models.py
+++ todo_app/tasks/models.py
@@ -21,6 +21,6 @@ class Task(models.Model):
         related_name="tasks",
         null=True,
     )
-    #completed = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.title
