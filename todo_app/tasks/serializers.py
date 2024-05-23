5e0772b82e91d658e464140856f0240cd1705088
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-05-23 16:27:47 -0500

    feat: creation of the create method of the TaskSerialaizer class

--- todo_app/tasks/serializers.py
+++ todo_app/tasks/serializers.py
@@ -11,3 +11,7 @@ class TasksSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = ["title", "description", "status", "user"]
+
+    def create(self, validated_data):
+        validated_data['user'] =self.context['request'].user
+        return super(TasksSerializer, self).create(validated_data)
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    #"django_filters",
+    "django_filters",
     "tasks.apps.TasksConfig",
 ]
 
