a9aa009e50e2c58d894e2cf25eb6c0bd17b6c154
Frd1029 <freduar1029@gmail.com>
2024-05-23 16:44:52 -0500

    chore: Debugging file imports.

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
 
 # Create your views here.
-from .models import Task
-from django.http import JsonResponse
-from django.core.serializers import serialize
-from .serializers import TasksSerializer
 from rest_framework import viewsets
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
+
+from .models import Task
+from .serializers import TasksSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -20,11 +16,3 @@ class TaskViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.request.user.tasks.all()
 
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        data["user"] = self.request.user
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CRATED, headers=headers)
