b9f07c859a668fcebb351a10710ff886ea80d3a9
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-05-23 16:56:34 -0500

    chore: addition of DEFAULT_AUTHENTICATION_CLASSES and replacement of DEFAULT_PERMISSION_CLASSES in settings.py file

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -128,8 +128,11 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication'
+    ],
     'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
