215fcea25a38cf084eeb437922a859d8c8e45f85
Frd1029 <freduar1029@gmail.com>
2024-05-28 09:49:29 -0500

    feat: Register the app profile in the admin.

--- todo_app/profiles/admin.py
+++ todo_app/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'username', 'email', 'is_superuser']
--- todo_app/tasks/admin.py
+++ todo_app/tasks/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 from .models import Task
 
 admin.site.register(Task)
-# admin.site.register(User)
+
