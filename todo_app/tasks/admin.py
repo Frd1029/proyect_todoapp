63e6085ba2047c730c3b1453880f5a7cc3d51714
Frd1029 <freduar1029@gmail.com>
2024-05-30 08:39:32 -0500

    chore: Task model registry fix.

--- todo_app/tasks/admin.py
+++ todo_app/tasks/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Task
 
-admin.site.register(Task)
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'status', 'deadline', 'priority', 'user']
     
