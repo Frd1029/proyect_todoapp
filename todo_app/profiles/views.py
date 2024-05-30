58ee9ec3d7bd5c8fc836fdcd3f9cceb8f7d4dc14
Frd1029 <freduar1029@gmail.com>
2024-05-30 08:40:54 -0500

    feat: Creation of the profile creation view.

--- todo_app/profiles/views.py
+++ todo_app/profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+
+from .serializers import ProfileCreationSerializer
 
 # Create your views here.
+
+class ProfileCreationView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = ProfileCreationSerializer
+    permission_classes = [AllowAny]
