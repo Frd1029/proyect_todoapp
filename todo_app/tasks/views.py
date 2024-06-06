3f17dd1db43a1fcb8db8a487cb1a62811da49ae3
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-06-06 10:25:28 -0500

    chore: settings for renaming TasksSerializer to TasksCreateSerializer

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -4,12 +4,12 @@ from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter
 
 from .models import Task
-from .serializers import TasksSerializer
+from .serializers import TasksCreateSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
-    serializer_class = TasksSerializer
+    serializer_class = TasksCreateSerializer
     filter_backends = [OrderingFilter]
     ordening_fields = ["priority"]
 
