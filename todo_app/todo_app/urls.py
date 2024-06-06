94df1e9a7f0dc946266e55255f01dca64d1cf9da
Frd1029 <freduar1029@gmail.com>
2024-06-06 16:48:02 -0500

    chore: Add "/" to endpoint auth/token/refresh/.

--- todo_app/todo_app/urls.py
+++ todo_app/todo_app/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("admin/", admin.site.urls)
 ]
