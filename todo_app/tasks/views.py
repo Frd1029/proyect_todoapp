5c41361fac4b0051933127e35c11868fb6bfddcd
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-06-06 11:41:56 -0500

    chore: implementation of the detroy method

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,10 +1,11 @@
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 from .models import Task
-from .serializers import TasksCreateSerializer, TaskStatusUpdateSerializer
+from .serializers import TasksCreateSerializer, TaskStatusUpdateSerializer, TaskDeleteSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -23,3 +24,12 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+            
+    def destroy(self, request, pk=None):
+        serializer = TaskDeleteSerializer(data={'id': pk})
+        serializer.is_valid(raise_exception=True)
+        queryset = self.get_queryset()
+        task = queryset.get(pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
