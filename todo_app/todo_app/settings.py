390a5b65fc80dba5c777a2eacd9975221e2eca58
Frd1029 <freduar1029@gmail.com>
2024-06-06 17:51:05 -0500

    chore: Applying password shortening.

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -92,19 +92,19 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", #Validar que la contraseña no sea muy similar a los atributos del usuario.
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", #Validar que la contraseña sea de min. 10 caracteres.
         'OPTIONS': {
-            'min_length': 8,
+            'min_length': 10,
         }
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", #Validar que la contraseña no sea muy común.
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", #Validar que la contraseña no sea completamente numerica.
     },
 ]
 
