8a3ff607c9a285525ee92cdcccc549a8902b262a
Frd1029 <freduar1029@gmail.com>
2024-06-06 15:52:16 -0500

    chore: Simple JWT is added to the project's installed applications.

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
     "django_filters",
     "profiles.apps.ProfilesConfig",
     "tasks.apps.TasksConfig",
