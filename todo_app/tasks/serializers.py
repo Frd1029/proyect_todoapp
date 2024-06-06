fbd8116982e80c8163dd5d86565414ee0d7a14d5
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-06-06 10:21:25 -0500

    chore: mplementation of the serializer to change the status field

--- todo_app/tasks/serializers.py
+++ todo_app/tasks/serializers.py
@@ -11,3 +11,13 @@ class TasksSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         validated_data['user'] =self.context['request'].user
         return super(TasksSerializer, self).create(validated_data)
+
+class TaskStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['status']
+        
+    def update(self, instance, validated_data):
+        instance.status = validated_data.get('status')
+        instance.save()
+        return instance
