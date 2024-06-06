b781d2e6f033d1b164cd764bd15c920d9fa171d7
Frd1029 <freduar1029@gmail.com>
2024-06-06 11:09:19 -0500

    chore: REST_FRAMEWORK authentication is configured to use JWT.

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated'
