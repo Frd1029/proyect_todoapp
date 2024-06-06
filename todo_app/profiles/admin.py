051ff8ea9e59b3203ea64085c527ae0832c39282
Frd1029 <freduar1029@gmail.com>
2024-06-06 11:13:25 -0500

    chore: User inheritance is changed to inherit from UserAdmin.

--- todo_app/profiles/admin.py
+++ todo_app/profiles/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Profile
 
 # Register your models here.
 
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(UserAdmin):
     list_display = ['id', 'first_name', 'last_name', 'username', 'email', 'is_superuser']
