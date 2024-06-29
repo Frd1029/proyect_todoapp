06b28fe6276165dbdeac54b664f43e8e4659b9f7
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-06-28 23:56:48 -0500

    chore: name error correction

--- todo_app/todo_app/settings/base.py
+++ todo_app/todo_app/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 ROOT_URLCONF = "todo_app.urls"
